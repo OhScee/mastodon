2a40a65ba2f0d493a7ef234123d2042ba6554d74
noscee <nspellen-cox@thrillworks.com>
2021-09-25 19:59:30 -0400

    style updates: SCSS and Images

--- app/javascript/mastodon/components/logo.js
+++ app/javascript/mastodon/components/logo.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Logo = () => (
-  <svg viewBox='0 0 216.4144 232.00976' className='logo'>
-    <use xlinkHref='#mastodon-svg-logo' />
-  </svg>
+  <svg width="798px" height="318px" viewBox="0 0 798 318" className="logo">
+    <title>tiny-space</title>
+    <g id="Page-1-light" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+        <g id="tiny-space" transform="translate(9.000000, 9.000000)" stroke="#F9CCC7" stroke-width="17">
+            <path d="M0,165 L240,165 C240,98.72583 186.27417,45 120,45 L120,285 C143.869484,285 166.761336,281.839295 183.63961,276.213203 C200.517884,270.587112 210,262.956495 210,255 L15,255 C23.2842712,255 30,174.411255 30,75 L210,75 M324,285 L324,150 C324,125.147186 317.284271,105 309,105 C317.284271,105 324,91.5685425 324,75 C324,91.5685425 337.431458,105 354,105 M420,120 C420,169.705627 500.588745,210 600,210 C699.411255,210 780,169.705627 780,120 M450,120 C450,53.72583 517.157288,0 600,0 C682.842712,0 750,53.72583 750,120 M450,195 C450,252.989899 517.157288,300 600,300 C682.842712,300 750,252.989899 750,195" id="Shape"></path>
+        </g>
+    </g>
+</svg>
 );
 
 export default Logo;
